79d9b9de49fa65957bcd8430f671bc7a8e552da0
Katie Emelianova <katieemelianova@Katies-MacBook-Air.local>
2022-10-28 17:15:40 +0200

    first commit

---
+++ dactylorhiza_functions.R
@@ -0,0 +1,65 @@
+
+
+# make a function which takes the original full data and gives back the counts and sampke info in a nmaed list based on selection statement
+specify_comparison<-function(samples, counts, selection_statement){
+  samples_return<-samples %>% filter(rlang::eval_tidy(rlang::parse_expr(selection_statement)))
+  counts_return <- counts %>% dplyr::select(rownames(samples_return))
+  return(list(samples=samples_return, counts=counts_return))
+}
+
+
+run_diffexp<-function(comparison, design_term, gene_lengths){
+  formula_parsed<-paste("~", design_term)
+  dds <- DESeqDataSetFromMatrix(countData = comparison[["counts"]],
+                                colData = comparison[["samples"]],
+                                design = as.formula(formula_parsed))
+  mcols(dds)$basepairs<-gene_lengths
+  dds <- estimateSizeFactors(dds)
+  idx <- rowSums( counts(dds, normalized=TRUE) >= 8 ) >= 5
+  dds <- dds[idx,]
+  dds <- DESeq(dds)
+  res <- results(dds)
+  return(list(dds=dds, results=res))
+}
+
+
+get_enriched_terms<-function(gene_list, mappings){
+  # use the gene 2 GOterms mapping provided for D. incarnata
+  geneID2GO<-mappings
+  # the input genes form the input, use these to annotate all genes, 1 is present in input list, 0 is absent
+  geneSel<-gene_list
+  geneSel<-factor(as.integer(names(geneID2GO) %in% geneSel))
+  names(geneSel)<-names(geneID2GO)
+  
+  # set up the topGO object
+  sampleGOdata <- new("topGOdata",
+                      ontology = "BP",
+                      allGenes = geneSel, 
+                      nodeSize = 10,
+                      annot = annFUN.gene2GO,
+                      gene2GO = geneID2GO)
+  
+  # run three tests, fisher, Kol-Smirn, and Kol-Smirn with elimination
+  resultFisher <- runTest(sampleGOdata, algorithm = "classic", statistic = "fisher")
+  resultKS <- runTest(sampleGOdata, algorithm = "classic", statistic = "ks")
+  resultKS.elim <- runTest(sampleGOdata, algorithm = "elim", statistic = "ks")
+  
+  # generate summary tane and return it
+  allRes <- GenTable(sampleGOdata, classicFisher = resultFisher,
+                     classicKS = resultKS, elimKS = resultKS.elim,
+                     orderBy = "classicFisher", ranksOf = "classicFisher", topNodes = 10)
+  #allRes<-GenTable(sampleGOdata, Fis = resultFisher, topNodes = 20)
+  return(allRes)
+}
+
+draw_heatmap<-function(dds_object){
+  dds_fpkm<-fpkm(dds_object$dds)
+  new_column_order<-dds_fpkm %>% colnames %>% sort()
+  dds_fpkm <- dds_fpkm %>% data.frame() %>% dplyr::select(new_column_order)
+  dds_significant<-dds_object$results %>% data.frame() %>% filter(log2FoldChange > 2 & padj < 0.0005) %>% rownames()
+  pheatmap(dds_fpkm[rownames(dds_fpkm) %in% dds_significant,], 
+           scale = "row", 
+           cluster_cols = FALSE,
+           cluster_rows = FALSE,
+           show_rownames = FALSE)
+}
---
+++ dactylorhiza_sample_info.R
@@ -0,0 +1,222 @@
+
+library(dplyr)
+library(magrittr)
+library(readr)
+library(stringr)
+library(tidyverse)
+library(DESeq2)
+library(topGO)
+library(pheatmap)
+source("/Users/katieemelianova/Desktop/Dactylorhiza/dactylorhiza_functions.R")
+
+# read in table: I got the same names from the RootRNAseq multiplex1 and multiplex2 dirs
+root_samples<-read.table("/Users/katieemelianova/Desktop/Dactylorhiza/Root_samples.txt", header=FALSE, col.names = c("sample_id"))
+
+# create new columns detailing  the species, environemt, tissue, assay and location
+root_samples$species<-case_when(substr(root_samples$sample_id,1,1) == "m" ~ "majalis",
+          substr(root_samples$sample_id,1,1) == "t" ~ "traunsteineri")
+
+root_samples$environment<-case_when(substr(root_samples$sample_id,2,2) == "M" ~ "majalis",
+                                    substr(root_samples$sample_id,2,2) == "T" ~ "traunsteineri")
+
+root_samples$locality<-case_when(substr(root_samples$sample_id,3,3) == "S" ~ "St Ulrich",
+                                 substr(root_samples$sample_id,3,3) == "K" ~ "Kitzbuhl")
+
+root_samples$replicate<-substr(root_samples$sample_id,4,4)
+
+root_samples$tissue<-case_when(substr(root_samples$sample_id,5,5) == "l" ~ "leaf",
+                                 substr(root_samples$sample_id,5,5) == "r" ~ "root")
+
+root_samples$assay<-case_when(endsWith(root_samples$sample_id, "_s") == TRUE ~ "sRNA",
+          endsWith(root_samples$sample_id, "_s") == FALSE ~ "RNAseq")
+
+# now we can filter by the tissue and assay: I think a few repeat libraries from leaf were sequenced along with the root samples
+root_samples %>% filter(tissue == "root" & assay == "RNAseq") %>% dplyr::select(sample_id)
+
+
+#################################################################################################
+#   At this point the sample names are extracted and used to process the fastqs on lisc         #
+#                         STAR mapping, featurecounts and fastqc                                #
+#   Now reading the featurecounts back in here for DEseq differential expression                #
+#################################################################################################
+
+
+
+# read in each featurecounts file and join them together
+#setwd("Desktop/Dactylorhiza/dactylorhiza_root_featurecounts/")
+df <- list.files(path='/Users/katieemelianova/Desktop/Dactylorhiza/dactylorhiza_root_featurecounts') %>% 
+  lapply(read_tsv, skip=1) %>% 
+  purrr::reduce(left_join, by = c("Geneid", "Chr", "Start", "End", "Strand", "Length"))
+
+# get and store the first standard featurecounts column names
+fc_cols<-colnames(df)[1:6]
+
+# take the 7th until the last colnames of the data frame
+sample_names<-colnames(df)[7:length(colnames(df))]
+sample_names<-str_replace(sample_names, "root_samples/", "")
+sample_names<-str_replace(sample_names, "Aligned.sortedByCoord.out.bam", "")
+
+# now set the df colnames to the shortened sample names for ease of reading
+colnames(df)<-c(fc_cols, sample_names)
+
+# get the relevant columns (geneid and sample counts) and set the geneid column to be rownames
+df_counts<-df %>% 
+  dplyr::select(c(1,7:length(colnames(df)))) %>% 
+  column_to_rownames("Geneid")
+
+
+#make the sample table, setting the sample id as the rowname
+root_samples %<>% filter(tissue == "root" & assay == "RNAseq") %>% column_to_rownames("sample_id")
+
+# remove irrelevant columns and add a transplant column to be used in the design
+root_samples %<>% mutate(treatment=case_when(species == environment ~ "native",
+                                            species != environment ~ "transplant")) %>% 
+  dplyr::select(-c(tissue, "assay"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#########################################################################
+#   baseline DE genes between native majalis and native traunsteineri   #
+#########################################################################
+
+# we want to find out what genes are DE between majalis and traunsteineri in their native areas per locality
+# one comparison for native majalis and traunsteineri in St Ulrich 
+# one comparison for the same but in Kitzbuhl
+
+native_kitzbuhl<-specify_comparison(root_samples, 
+                                    df_counts, 
+                                    "treatment == 'native' & locality == 'Kitzbuhl'") %>% 
+  run_diffexp("species", df$Length)
+
+
+native_stulrich<-specify_comparison(root_samples, 
+                                    df_counts, 
+                                    "treatment == 'native' & locality == 'St Ulrich'") %>% 
+  run_diffexp("species", df$Length)
+
+one<-native_kitzbuhl$results %>% data.frame() %>% filter(log2FoldChange > 2 & padj < 0.05) %>% rownames()
+two<-native_stulrich$results %>% data.frame() %>% filter(log2FoldChange > 2 & padj < 0.05) %>% rownames()
+
+
+length(one)
+length(two)
+intersect(one, two)
+
+
+
+############################################################################################
+#        DE genes in each species when transplanted compared to native, per locality       #
+############################################################################################
+
+
+transplant_majalis_kitzbuhl<-specify_comparison(root_samples, df_counts, 'species == "majalis" & locality == "Kitzbuhl"') %>% run_diffexp("treatment", df$Length)
+transplant_majalis_stulrich<-specify_comparison(root_samples, df_counts, 'species == "majalis" & locality == "St Ulrich"') %>% run_diffexp("treatment", df$Length)
+transplant_traunsteineri_kitzbuhl<-specify_comparison(root_samples, df_counts, 'species == "traunsteineri" & locality == "Kitzbuhl"') %>% run_diffexp("treatment", df$Length)
+transplant_traunsteineri_stulrich<-specify_comparison(root_samples, df_counts, 'species == "traunsteineri"& locality == "St Ulrich"') %>% run_diffexp("treatment", df$Length)
+
+
+one<-transplant_majalis_kitzbuhl$results %>% data.frame() %>% filter(log2FoldChange > 2 & padj < 0.05) %>% rownames()
+two<-transplant_majalis_stulrich$results %>% data.frame() %>% filter(log2FoldChange > 2 & padj < 0.05) %>% rownames()
+three<-transplant_traunsteineri_kitzbuhl$results %>% data.frame() %>% filter(log2FoldChange > 2 & padj < 0.05) %>% rownames()
+four<-transplant_traunsteineri_stulrich$results %>% data.frame() %>% filter(log2FoldChange > 2 & padj < 0.05) %>% rownames()
+
+
+test<-sapply(one, function(x) str_split(x, ":")[[1]][1]) %>% unname()
+
+mp[[test[2]]]
+
+test %in% names(mp)
+
+intersect(one, two)
+intersect(three, four)
+
+
+################################################################
+#               GO term enrichment analysis                    #
+################################################################
+
+mp<-readMappings("/Users/katieemelianova/Desktop/Dactylorhiza/data/all_annotations_justGO.txt")
+
+one<-sapply(one, function(x) str_split(x, ":")[[1]][1]) %>% unname()
+two<-sapply(two, function(x) str_split(x, ":")[[1]][1]) %>% unname()
+
+three<-sapply(three, function(x) str_split(x, ":")[[1]][1]) %>% unname()
+four<-sapply(four, function(x) str_split(x, ":")[[1]][1]) %>% unname()
+
+
+majalis_kitzbuhl_trans<-get_enriched_terms(one, mp)
+majalis_stulrich_trans<-get_enriched_terms(two, mp)
+
+traunsteineri_kitzbuhl_trans<-get_enriched_terms(three, mp)
+traunsteineri_sturich_trans<-get_enriched_terms(four, mp)
+
+
+################################################################
+#                          plot heatmaps                       #
+################################################################
+
+# make a dds object from the total root samples (no subsetting)
+root_dds<-specify_comparison(root_samples, df_counts, "1 == 1")
+root_dds <- DESeqDataSetFromMatrix(countData = root_dds[["counts"]],
+                                   colData = root_dds[["samples"]],
+                                   design = ~ treatment)
+
+
+
+
+draw_heatmap(transplant_majalis_kitzbuhl)
+draw_heatmap(transplant_majalis_stulrich)
+draw_heatmap(transplant_traunsteineri_kitzbuhl)
+draw_heatmap(transplant_traunsteineri_stulrich)
+
+
+
+test<-fpkm(transplant_majalis_kitzbuhl$dds) %>% colnames %>% sort()
+fpkm(transplant_majalis_kitzbuhl$dds) %>% data.frame() %>% dplyr::select(test) %>% colnames()
+
+
+test<-fpkm(transplant_majalis_kitzbuhl$dds)
+test_res<-transplant_majalis_kitzbuhl$results %>% data.frame() %>% filter(log2FoldChange > 2 & padj < 0.05) %>% rownames()
+
+stress<-c("Dinc068413-RA",
+          "Dinc122846-RA")
+pheatmap(test[rownames(test) %in% stress,], scale = "row", cluster_cols = FALSE)
+
+
+mcols(root_dds)$basepairs<-df$Length
+
+root_dds <- estimateSizeFactors(root_dds)
+idx <- rowSums( counts(root_dds, normalized=TRUE) >= 5 ) >= 3
+root_dds <- root_dds[idx,]
+
+# add in a gene length column for FPKM calculation
+
+root_fpkms<-fpkm(root_dds)
+
+root_fpkms[rownames(root_fpkms) %in% three,]
+
+root_fpkms %>% filter(rownames(root_fpkms) %in% two)
+pheatmap(root_fpkms[rownames(root_fpkms) %in% one,], scale = "row", cluster_cols = FALSE)
+
+head(root_fpkms, 1000)
+
+
+
