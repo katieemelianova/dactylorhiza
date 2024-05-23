c0ea81a358e7056b6512075c847bf9ca396eb416
Katie Emelianova <katieemelianova@Katies-MacBook-Air.local>
2024-05-23 07:45:31 +0200

    edit abundance ploit

--- plot_fungal_abundance.R
+++ plot_fungal_abundance.R
@@ -9,12 +9,15 @@ library(ggplot2)
 library(reshape2)
 library(viridis)
 library(RColorBrewer)
+library(ggh4x)
+library(Polychrome)
+library(ggpubr)
 
 
 read_tax<-function(file, sampleid, rank, full_taxonomy=FALSE){
   exam<-read_tsv(file) %>% mutate(sample=sampleid) %>% filter(RANK == rank)
   if(full_taxonomy == FALSE){ 
-    exam %<>% separate(TAXPATHSN, c(NA, NA, NA, NA, "B", NA))
+    exam %<>% separate(TAXPATHSN, c(NA, NA, NA, "B", NA, NA))
   }
   return(exam)
 }
@@ -61,6 +64,8 @@ mMK3r_data<-read_tax("/Users/katieemelianova/Desktop/Dactylorhiza/abundances/mMK
 
 
 
+samples_to_exclude<-c("mTS2r", "tTS4r", "tTK1r", "tMK4r")
+
 
 
 all<-rbind(mTS2r_data,
@@ -100,17 +105,53 @@ all<-rbind(mTS2r_data,
            tTS3r_data,
            mMK3r_data
            ) %>% 
-  filter(B != "Unclassified") %>%
+  #filter(B != "Unclassified" & !(sample %in% samples_to_exclude)) %>%
+  filter(!(sample %in% samples_to_exclude)) %>%
   dplyr::select(B, PERCENTAGE, sample, Species, Environment) %>%
-  filter(PERCENTAGE > 5) %>%
+  mutate(B2 = case_when(B == "Unclassified" ~ "Other",
+                        PERCENTAGE >= 1 ~ B,
+                        PERCENTAGE <= 1 ~ "Other",
+                        )) %>%
   melt()
+
+# rename enviorinment to be shorter
+all %<>% mutate(Environment = ifelse(Environment == "D. majalis environment", "M", "T"))
+# order orders, put other at the end
+order_of_orders=(c(unique(all$B2)[which(unique(all$B2) != "Other")], "Other"))
+all %<>% 
+  arrange(B2) %>% 
+  mutate(B2 = factor(B2, levels=order_of_orders))
+
+
+
+
+
+#####################################################################
+#     define matching colour scheme for bar and pie charts          #
+#####################################################################
+
+
+# I used these lines to pick seed colours for 
+#qual_col_pals = brewer.pal.info[rownames(brewer.pal.info) %in% c("Spectral"),]
+#col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+
+P36 = createPalette(36,  c("#5E4FA2", "#3288BD", "#66C2A5")) %>% as.character()
+
+# get as many colours as you need, minus one to put grey at the end for Other
+col_vector<-c(P36[1:length(levels(all$B2)) -1], "gray63")
+
+
+
 
 ##################################################
 #            plot abundances barplot             #
 ##################################################
 
-png(file="~/Desktop/Dactylorhiza/dactylorhiza/Figure9.png", width = 1100, height = 500)
-ggplot(data=all, aes(x=sample, y=value, fill=B)) +
+strip <- strip_themed(background_x = elem_list_rect(fill = c("gold", "gold", "dodgerblue", "dodgerblue", "gold", "dodgerblue", "gold", "dodgerblue")))
+
+#png(file="~/Desktop/Dactylorhiza/dactylorhiza/Figure7.png", width = 1600, height = 700)
+pdf(file="~/Desktop/Dactylorhiza/dactylorhiza/Figure7.pdf", height=20, width=45)
+ggplot(data=all, aes(x=sample, y=value, fill=B2), colour=B2) +
   geom_bar(stat="identity") +
   theme(axis.text.x = element_blank(),
         axis.text.y = element_blank(),
@@ -118,39 +159,42 @@ ggplot(data=all, aes(x=sample, y=value, fill=B)) +
         axis.ticks.x = element_blank(),
         axis.ticks.y = element_blank(),
         strip.text = element_text(
-          size = 13,
-          face="bold"),
+          size = 50),
         panel.grid = element_blank(),
         panel.border = element_blank(),
-        legend.text=element_text(size=13),
-        legend.title=element_text(size=15, face="bold"),
-        axis.title.y = element_text(size = 15)) +
+        legend.text=element_text(size=50),
+        legend.title=element_text(size=60, face="bold"),
+        axis.title.y = element_text(size = 65)) +
   ylab("% Abundance") +
   labs(fill = "Fungal order") +
-  facet_wrap(~ Species + Environment, scales = "free", ncol=4)+
-  scale_y_continuous(limits = c(0,100), expand = expansion(mult = c(0, 0)))
+  facet_wrap2(~ Species + Environment, scales = "free", strip = strip, ncol=4) +
+  #facet_wrap(~ Species + Environment, scales = "free", ncol=4)+
+  #scale_y_continuous(limits = c(0,100), expand = expansion(mult = c(0, 0))) +
+  scale_fill_manual(values=col_vector[1:20], name = "Fungal order")
 dev.off()
 
 
 
 
 
+# calculation of average non-Wallemiales taxa
+all %>% filter(Environment == "D. majalis environment" & Species == "D. majalis" & B != "Wallemiales") %>% dplyr::select(c("sample", "value")) %>% aggregate(.$value ~.$sample, ., FUN=sum) %>% set_colnames(c("sample", "value")) %>% dplyr::select("value") %>% pull() %>% mean()
+all %>% filter(Environment == "D. traunsteineri environment" & Species == "D. majalis" & B != "Wallemiales") %>% dplyr::select(c("sample", "value")) %>% aggregate(.$value ~.$sample, ., FUN=sum) %>% set_colnames(c("sample", "value")) %>% dplyr::select("value") %>% pull() %>% mean()
+all %>% filter(Environment == "D. majalis environment" & Species == "D. traunsteineri" & B != "Wallemiales") %>% dplyr::select(c("sample", "value")) %>% aggregate(.$value ~.$sample, ., FUN=sum) %>% set_colnames(c("sample", "value")) %>% dplyr::select("value") %>% pull() %>% mean()
+all %>% filter(Environment == "D. traunsteineri environment" & Species == "D. traunsteineri" & B != "Wallemiales") %>% dplyr::select(c("sample", "value")) %>% aggregate(.$value ~.$sample, ., FUN=sum) %>% set_colnames(c("sample", "value")) %>% dplyr::select("value") %>% pull() %>% mean()
 
 
+# calculation of how much wallemiales in each comparison
+all %>% filter(Environment == "D. majalis environment" & Species == "D. majalis" & value > 1 & B == "Wallemiales") %>% dplyr::select(c("sample", "value")) %>% aggregate(.$value ~.$sample, ., FUN=sum) %>% set_colnames(c("sample", "value")) %>% dplyr::select("value") %>% pull() %>% mean()
+all %>% filter(Environment == "D. traunsteineri environment" & Species == "D. majalis" & value > 1 & B == "Wallemiales") %>% dplyr::select(c("sample", "value")) %>% aggregate(.$value ~.$sample, ., FUN=sum) %>% set_colnames(c("sample", "value")) %>% dplyr::select("value") %>% pull() %>% mean()
+all %>% filter(Environment == "D. majalis environment" & Species == "D. traunsteineri" & value > 1 & B == "Wallemiales") %>% dplyr::select(c("sample", "value")) %>% aggregate(.$value ~.$sample, ., FUN=sum) %>% set_colnames(c("sample", "value")) %>% dplyr::select("value") %>% pull() %>% mean()
+all %>% filter(Environment == "D. traunsteineri environment" & Species == "D. traunsteineri" & value > 1 & B == "Wallemiales") %>% dplyr::select(c("sample", "value")) %>% aggregate(.$value ~.$sample, ., FUN=sum) %>% set_colnames(c("sample", "value")) %>% dplyr::select("value") %>% pull() %>% mean()
 
-#all_temp<-all_5pct %>% mutate(V5 = case_when(species == "majalis" ~ value*(-1),
-#                              species == "traunsteineri" ~ value
-#                      ))
-#brks <- seq(-15000000, 15000000, 5000000)
-#lbls = paste0(as.character(c(seq(15, 0, -5), seq(5, 15, 5))), "m")
-#
-#ggplot(all_temp, aes(x = B, y = V5, fill = species)) +
-#  geom_bar(stat = "identity", width = .6) +   # draw the bars
-#  scale_y_continuous(breaks = brks,
-#                     labels = lbls) +
-#  coord_flip() +  # Flip axes
-#  scale_fill_brewer(palette = "Accent") +  # Color palette
-#  facet_wrap(~environment)
+
+##################################################################
+#               Pie charts (might not include these)             #
+##################################################################
+
 
 
 all$species<-case_when(substr(all$sample,1,1) == "m" ~ "majalis",
@@ -164,29 +208,103 @@ all$environment<-case_when(substr(all$sample,2,2) == "M" ~ "majalis",
 
 env_labs <- c("D. traunsteineri environment", "D. majalis environment")
 names(env_labs) <- c("traunsteineri", "majalis")
+
+env_labs_blank <- c("", "")
+names(env_labs_blank) <- c("traunsteineri", "majalis")
+
 sp_labs <- c("D. traunsteineri", "D. majalis")
 names(sp_labs) <- c("traunsteineri", "majalis")
 
-#pie_all<-aggregate(all$value, by=list(Category=all$B), FUN=sum)
-pie_all<-aggregate(all$value ~all$B+species+locality+environment, all, FUN=sum)
-colnames(pie_all)<-c("B", "species", "locality", "environment", "value")
+pie_all<-aggregate(all$value ~all$B2+species+locality+environment, all, FUN=mean)
+colnames(pie_all)<-c("B2", "species", "locality", "environment", "value")
+
+maj_pie<-pie_all %>% filter(environment == "majalis") %>% 
+  ggplot(aes(x="", y=value, fill=B2)) +
+  geom_bar(stat="identity", width=1, position = "fill") +
+  coord_polar("y", start=0) +
+  facet_wrap(~environment + species + locality, labeller = ggplot2::labeller(environment = env_labs_blank, species = sp_labs), ncol=2) +
+  theme_void() +
+  scale_fill_manual(values=col_vector[1:20], name = "Fungal order") +
+  theme(#strip.text.x = element_blank(),
+        strip.text.x = element_text(size = 13), 
+        legend.position = "none",
+        axis.text.x=element_blank(),
+        legend.text=element_text(size=13),
+        legend.title=element_text(size=15, face="bold"),
+        plot.background = element_rect(fill=alpha('gold', 0.2), colour="white"))
 
 
-qual_col_pals = brewer.pal.info[rownames(brewer.pal.info) %in% c("Dark2", "Spectral", "Paired", "PuOr"),]
-col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 
-png(file="~/Desktop/Dactylorhiza/dactylorhiza/Figure10.png", width = 900, height = 600)
-ggplot(pie_all, aes(x="", y=value, fill=B)) +
+tra_pie<-pie_all %>% filter(environment == "traunsteineri") %>% 
+  ggplot(aes(x="", y=value, fill=B2)) +
+  geom_bar(stat="identity", width=1, position = "fill") +
+  coord_polar("y", start=0) +
+  facet_wrap(~environment + species + locality, labeller = ggplot2::labeller(environment = env_labs_blank, species = sp_labs), ncol=2) +
+  theme_void() +
+  scale_fill_manual(values=col_vector[1:20], name = "Fungal order") +
+  theme(strip.text.x = element_text(size = 13), 
+        legend.position = "none",
+        axis.text.x=element_blank(),
+        legend.text=element_text(size=13),
+        legend.title=element_text(size=15, face="bold"),
+        plot.background = element_rect(fill=alpha('dodgerblue', 0.2), colour="white"))
+
+
+legend_plot<-ggplot(pie_all, aes(x="", y=value, fill=B2)) +
   geom_bar(stat="identity", width=1, position = "fill") +
   coord_polar("y", start=0) +
   facet_wrap(~environment + species + locality, labeller = ggplot2::labeller(environment = env_labs, species = sp_labs), ncol=4) +
   theme_void() +
-  scale_fill_manual(values=col_vector[1:38], name = "Fungal family") +
-  theme(strip.text.x = element_text(size = 13, face="bold"), 
-        axis.text.x=element_blank(),
-        legend.text=element_text(size=13),
+  scale_fill_manual(values=col_vector[1:20], name = "Fungal order") +
+  theme(legend.text=element_text(size=13),
         legend.title=element_text(size=15, face="bold"))
+
+leg <- cowplot::get_legend(legend_plot)
+leg<-as_ggplot(leg)
+
+
+grid_a<-cowplot::plot_grid(maj_pie, tra_pie, ncol = 1)
+#png(file="~/Desktop/Dactylorhiza/dactylorhiza/Figure8_v2.png", width = 800, height = 2000)
+pdf(file="~/Desktop/Dactylorhiza/dactylorhiza/Figure8_v2.pdf", height=30, width=30)
+cowplot::plot_grid(grid_a, NULL, 
+                   leg, ncol=3, 
+                   rel_widths = c(3, -1, 1),
+                   rel_heights = c(3, -1, 1))
 dev.off()
+
+
+
+##################################################################
+#                    Shannon Diversity index                     #
+##################################################################
+
+
+
+
+pie_all %>% filter(environment == "traunsteineri" & B2 == "Spizellomycetales")
+pie_all %>% filter(environment == "majalis" & B2 == "Spizellomycetales")
+
+
+pie_all %>% filter(environment == "traunsteineri" & B2 == "Wallemiales")
+pie_all %>% filter(environment == "majalis" & B2 == "Wallemiales")
+
+pie_all %>% filter(environment == "traunsteineri" & B2 == "Mortierellales")
+pie_all %>% filter(environment == "majalis" & B2 == "Mortierellales")
+
+
+Metschnikowiaceae<-c("4.95051140598397", "4.14190720823853", "3.75429195682506", "4.0666008024767", "7.79768874779775", "3.13711986917807", "4.25065818326258", "3.19785504237587", "2.15101319638948", "1.79415277851309", "11.4381103015332", "4.08167398824527", "5.71679467233381", "5.77298416061022", "5.84661267855805", "3.62544403139846", "3.40470192870115", "3.0021293822947", "5.65171167602384", "13.3713331029679", "4.60768721930311", "10.6179750634019", "5.11088233317787", "7.16394204051486", "2.0412422486734", "5.83200730326674", "7.97951064993315", "9.45357383821955", "2.11726229298103", "89.4459467113006", "5.03049809433911", "4.20752980303311", "6.91381717835442")
+Wallemiacetes<-c("59.1668055600235", "36.215710507056", "91.6381422089486", "37.7644107085191", "60.2817218353655", "29.8170541231346", "52.1817920427708", "89.6808540756771", "5.09064911206817", "4.80126153808208", "40.1337025619341", "0.647357012426202", "90.157455908127", "87.9530483735043", "63.1897303489549", "17.4025675282367", "89.8215713061064", "17.5676206314639", "64.3362659956943", "35.3479484510778", "46.4053243706007", "22.459840273908 13.4983058215499", "68.5944663413997", "13.762026347436 87.5202440883756", "77.3058008512956", "54.7379409580447", "22.0912617026523", "0.313134018027669", "83.0301613666154", "55.8514507087588", "81.4127642328277")
+
+Wallemiacetes<-c("59.1668055600235", "36.215710507056 91.6381422089486", "37.7644107085191", "60.2817218353655", "29.8170541231346", "52.1817920427708", "89.6808540756771", "5.09064911206817", "4.80126153808208", "40.1337025619341", "0.647357012426202", "90.157455908127 87.9530483735043", "63.1897303489549", "17.4025675282367", "89.8215713061064", "17.5676206314639", "64.3362659956943", "35.3479484510778", "46.4053243706007", "22.459840273908 13.4983058215499", "68.5944663413997", "13.762026347436 87.5202440883756", "77.3058008512956", "54.7379409580447", "22.0912617026523", "0.313134018027669", "83.0301613666154", "55.8514507087588", "81.4127642328277")
+
+Metschnikowiaceae %>% length()
+
+en<-c("M", "M", "M", "T", "M", "T", "M", "M", "T", "T", "M", "T", "M", "T", "M", "T", "M", "T", "T", "M", "T", "M", "T", "M", "T", "M", "M", "M", "T", "M", "T", "T", "M")
+test<-data.frame(m=Metschnikowiaceae, w=wall, e=en)
+
+test%>% filter(e=="T") %>% dplyr::select(m) %>% pull() %>% as.numeric() %>% mean()
+test%>% filter(e=="T") %>% dplyr::select(w) %>% pull() %>% as.numeric() %>% mean()
+
 
 #############################################################################
 #     statistical tests (repetitive code but needs to be a bit formatted)   #
@@ -296,8 +414,6 @@ env_line[1]<-"Environemt"
 colnames(all_stat_spread)[1]<-"Sample ID"
 
 all_stat_spread_annotated<-rbind(species_line, env_line, all_stat_spread) %>% na.omit()
-
-
 
 write_tsv(all_stat_spread_annotated, "/Users/katieemelianova/Desktop/Dactylorhiza/dactylorhiza/all_stat_spread_annotated.tsv")
 
